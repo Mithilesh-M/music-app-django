631d2a65f40353672f44cf1c588624ec7d4fe802
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-26 15:49:12 +0530

    Update filter in music list view
    Update filter name in music app

--- musicapp/filters.py
+++ musicapp/filters.py
@@ -2,8 +2,8 @@ import django_filters
 from .models import Music, Album, Band, Label, Genre
 
 class MusicFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    slug = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(lookup_expr='icontains',label='Title')
+    slug = django_filters.CharFilter(lookup_expr='icontains',label='Slug')
 
     class Meta:
         model = Music
