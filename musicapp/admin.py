d5225cf9a6ece1acc9af8e80c9a9a7a68ee6d1d1
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-26 11:46:44 +0530

    Register models in admin page for music app
    Add models in admin page

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Music,Album,Band,Label,Genre
 
-# Register your models here.
+admin.site.register(Music)
+admin.site.register(Album)
+admin.site.register(Band)
+admin.site.register(Label)
+admin.site.register(Genre)
