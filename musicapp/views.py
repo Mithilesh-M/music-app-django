eccfeecf73614962a23567c5bceffc2b8c4f2c2e
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-26 12:47:46 +0530

    Add index view for music app
    Add home page for music app

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Music, Album, Band, Label, Genre
 
-# Create your views here.
+
+def show_index(request):
+    context = {
+        'No_Of_Music': Music.objects.all().count(),
+        'No_Of_Album': Album.objects.all().count(),
+        'No_Of_Band': Band.objects.all().count(),
+        'No_Of_Label': Label.objects.all().count(),
+        'No_Of_Genre': Genre.objects.all().count(),
+    }
+    return render(request, 'musicapp/index.html', context)
